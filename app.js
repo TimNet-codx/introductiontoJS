3eb6a74fcdafcbd6dccef58523a7ba0dc842bc8d
Agbeniga Jesulayomi Timothy <82886562+TimNet-codx@users.noreply.github.com>
2022-06-23 15:56:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var name = "Agbeniga Jesulayomi Timothy";
+let height = 20;
+let country = "Nigeria";
+
+alert("Name: " + name + "  Height: "  + height + "  Country: " +  country);
+// console.log("Name: " + name + "  Height: "  + height + "  Country: " +  country);
+
+// document.getElementById("showDetails").innerHTML = "Name: " + name
+//                                                    + " Height: " + height +
+//                                                     " Country: " + country;
+
+
+
+ 
